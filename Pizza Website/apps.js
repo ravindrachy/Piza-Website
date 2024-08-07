7b2106ed9c11b19e82e9f1b70c44c14ca85c4077
Ravindra Kumar <157727250+ravindrachy@users.noreply.github.com>
2024-08-07 19:25:45 +0530

    Add files via upload

---
+++ Pizza Website/apps.js
@@ -0,0 +1,109 @@
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'Double Chesse Veggie (Onion + Capsicum + Tomato)',
+        image: '1.PNG',
+        price: 850 
+    },
+    {
+        id: 2,
+        name: 'Non Veg Pizza',
+        image: '2.PNG',
+        price: 1000
+    },
+    {
+        id: 3,
+        name: 'Cheese Veggie Pizza',
+        image: '3.PNG',
+        price: 500
+    },
+    {
+        id: 4,
+        name: 'Cheese Garden Pizza(Olives + Red Paprika)',
+        image: '4.PNG',
+        price: 770
+    },
+    {
+        id: 5,
+        name: 'Cheese Capsicum + Corn Pizza',
+        image: '5.PNG',
+        price: 660
+    },
+    {
+        id: 6,
+        name: 'Cheese Tomato Pizza',
+        image: '6.PNG',
+        price: 800
+    }
+];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Add To Card</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        // copy product form list to list card
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    }
+    reloadCard();
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="image/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
